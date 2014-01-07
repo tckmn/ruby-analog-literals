72e63cebddad079e35e1dae1869b7d9b82bb432a
OddLlama <oddllamaprod@gmail.com>
2014-01-06 20:50:08 -0600

    Create analog-literals.rb

---
+++ analog-literals.rb
@@ -0,0 +1,30 @@
+class Module
+    def const_missing c
+        Measurer.new 0
+    end
+end
+
+class Measurer
+    attr_accessor :val, :bangs
+    def initialize val, bangs = 0
+        @val = val
+        @bangs = bangs
+    end
+
+    # 1D
+    def -@
+        Measurer.new @val + 1, @bangs
+    end
+    def - x
+        Measurer.new @val + x.val + 1, @bangs
+    end
+
+    # 2D
+    def !@
+        Measurer.new @val, @bangs + 1
+    end
+
+    def to_s
+        (@val * (@bangs == 0 ? 1 : @bangs / 2)).to_s
+    end
+end
